2e2e35b03fc8bd41745aabd6a885d6601e696e17
theonlinedragon <83085124+theonlinedragon@users.noreply.github.com>
2021-11-01 23:36:27 -0500

    updated windows for scrollbar

---
+++ GUI1.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+window = Tk()
+open = Button(window, text="last")
+close = Button(window, text="first")
+open.pack()
+close.pack()
+window.mainloop()
---
+++ GUI2.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+window = Tk()
+open = Button(window, text="Open")
+close = Button(window, text="different")
+open.pack()
+close.pack()
+window.mainloop()
