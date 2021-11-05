658aa5f1a77b75e8cfc619f34cbc36105a2e5ab4
theonlinedragon <83085124+theonlinedragon@users.noreply.github.com>
2021-11-05 13:46:37 -0500

    opens a 2nd window after window 1

--- GUI1.py
+++ GUI1.py
@@ -1,8 +1,14 @@
+import tkinter as tk
 from tkinter import *
 
+def cry():
+    i=10
+    while (i>0):
+        i-=1
+        print("tears falling down")
 window = Tk()
 open = Button(window, text="last")
-close = Button(window, text="first")
+close = Button(window, text="first", command=cry)
 open.pack()
 close.pack()
 window.mainloop()
--- GUI2.py
+++ GUI2.py
@@ -1,8 +1,23 @@
 from tkinter import *
+import random
+
+def something():
+    print("working code")
+
+def opposite():
+    i=10
+    j=0
+    while (j != i):
+        t=1
+        print(t)
+        t +=1
+        j +=1
+        print(j)
+
 
 window = Tk()
-open = Button(window, text="Open")
-close = Button(window, text="different")
+open = Button(window, text="Open", command=something)
+close = Button(window, text="different", command=opposite)
 open.pack()
 close.pack()
 window.mainloop()
