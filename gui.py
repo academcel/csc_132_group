70819fe03f9e57ee6f1982a31f5370e2d4630612
Dak <dakotadivine.education@gmail.com>
2021-11-08 12:08:07 -0600

    Messy gui

--- gui.py
+++ gui.py
@@ -1,13 +1,28 @@
 from tkinter import *
-DEBUG = FALSE
+DEBUG = False
 
 window = Tk()
 if DEBUG:
     window.attributes("-fullscreen", True)
-frame1 = Frame(window, background = "red")
-frame1.pack
+window.geometry("800x480")
 
+left_frame = Frame(window, width=266, height=480, bg='cyan')
+left_frame.pack(side=LEFT)
 
+right_frame = Frame(window, width=266, height=480, bg="red")
+right_frame.pack(side=RIGHT)
+
+middle_frame = Frame(window, width=266, height=480, bg="yellow")
+middle_frame.pack()
+
+left_btn = Button(left_frame, text="Curtains")
+left_btn.pack(padx=106, pady=229)
+
+right_btn = Button(right_frame, text="Lights")
+right_btn.pack(padx=106, pady=229)
+
+middle_btn = Button(middle_frame, text="Plant")
+middle_btn.pack(padx=115, pady=229)
 
 window.mainloop()
 
